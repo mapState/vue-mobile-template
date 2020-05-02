46cd75441c4d3c7e6d2994e4e59ccfc16d800071
mapState <feimtui0@163.com>
2020-05-02 14:27:30 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
+import fastclick from 'fastclick';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import "amfe-flexible/index.js"
+import { vueBaberrage} from 'vue-baberrage'
+import {Button,Field,Toast,Dialog } from 'vant';
+[vueBaberrage,Button,Field,Toast,Dialog].forEach(e => {
+  Vue.use(e)
+});
+Vue.use(VueAxios,axios);
+// Vue.config.productionTip = false
+// fastclick.attach(document.body);
+// axios.defaults.baseURL = '/api';
+// axios.interceptors.response.use(function(response){
+//   let res = response.data;
+//   if(res.status == 0){
+//     return res.data;
+//   }else if(res.status == 10){
+//     return Promise.reject(res);
+//   }else{
+//     Message.warning(res.msg);
+//     return Promise.reject(res);
+//   }
+// },(error)=>{
+//   let res = error.response;
+//   this.$toast.fail('失败文案');
+//   return Promise.reject(error);
+// });
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-baberrage :isShow="barrageIsShow" :barrageList="barrageList" :loop="barrageLoop">
+      <template v-slot:default="slotProps">
+        <span style="color: #f60">{{slotProps.item.msg}}</span>
+      </template>
+    </vue-baberrage>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+/**
+ * vue-baberrage 文档
+ * https://github.com/superhos/vue-baberrage/blob/master/docs/zh/README.md#plugin-options
+ */
+import { MESSAGE_TYPE } from "vue-baberrage";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      msg: "Hello vue-baberrage",
+      barrageIsShow: true,
+      currentId: 0,
+      barrageLoop: false,
+      barrageList: []
+    };
+  },
+  mounted() {
+    this.addToList();
+  },
+  methods: {
+    addToList() {
+      for (let i = 0; i <= 100; i++) {
+        this.barrageList.push({
+          id: ++this.currentId,
+          avatar:
+            "https://hbimg.huabanimg.com/f217037368b35201aa8344e79e4c193591af7c574a0bb-WwKqvG_fw658/format/webp",
+          msg: this.msg + i + "",
+          time: i % 10,
+          type: MESSAGE_TYPE.NORMAL
+        });
+      }
+    }
   }
-}
+};
 </script>
+<style>
+</style>
